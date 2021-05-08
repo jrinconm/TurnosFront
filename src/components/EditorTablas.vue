74d7c6e7454ac56b453c95cc518d345e00faee5a
Julian <julianrinconm@gmail.com>
2021-05-08 12:27:27 +0200

    Crud Departametnos, estados, roles

---
+++ src/components/EditorTablas.vue
@@ -0,0 +1,184 @@
+<!-- Aqui template, donde ira el HTML que Vue renderizara -->
+<template>
+  <div class="q-pa-md">
+    <q-table
+      :title="tabla.nombre"
+      :data="data"
+      row-key="name"
+      binary-state-sort
+      :columns="columns"
+    >
+      <template v-slot:top-right>
+        <q-btn
+          align="right"
+          dense
+          color="secondary"
+          label="Añadir un item"
+          @click="show_dialog = true"
+          no-caps
+        ></q-btn>
+        <div class="q-pa-sm q-gutter-sm">
+          <q-dialog v-model="show_dialog">
+            <q-card>
+              <q-card-section>
+                <div class="text-h6">Añadir un item</div>
+              </q-card-section>
+
+              <q-card-section>
+                <div class="row">
+                  <q-input v-model="editedItem.name" label="Nombre"></q-input>
+                </div>
+              </q-card-section>
+
+              <q-card-actions align="right">
+                <q-btn
+                  flat
+                  label="OK"
+                  color="primary"
+                  v-close-popup
+                  @click="addRow"
+                ></q-btn>
+              </q-card-actions>
+            </q-card>
+          </q-dialog>
+        </div>
+      </template>
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td key="descripcion" :props="props">
+            {{ props.row.descripcion }}
+            <q-popup-edit v-model="props.row.descripcion">
+              <q-input
+                v-model="props.row.descripcion"
+                dense
+                autofocus
+                counter
+              />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="actions" :props="props">
+            <q-btn
+              class="q-ma-sm"
+              color="blue"
+              label="Actualizar"
+              @click="editItem(props.row)"
+              size="sm"
+              no-caps
+            ></q-btn>
+            <q-btn
+              color="red"
+              label="Borrar"
+              @click="deleteItem(props.row)"
+              size="sm"
+              no-caps
+            ></q-btn>
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
+  </div>
+</template>
+<!-- Aqui script, donde irá el Javascript (métodos, funciones, etc) -->
+<script>
+import { api } from "boot/axios";
+export default {
+  name: "editortablas",
+  props: ["tabla"],
+  data() {
+    return {
+      data: [],
+      show_dialog: false,
+      editedIndex: -1,
+      editedItem: {
+        name: ""
+      },
+      defaultItem: {
+        name: ""
+      },
+      //visibleColumns: ["descripcion", "actions"],
+      columns: [
+        {
+          name: "descripcion",
+          required: true,
+          label: "DESCRIPCIÓN",
+          align: "left",
+          field: row => row.name,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: "actions",
+          label: "Acciones",
+          field: "actions"
+        }
+      ]
+    };
+  },
+  computed: {
+    JWTToken: function() {
+      return localStorage.getItem("user-token");
+    }, // store the token in localstorage
+    id: function() {
+      return localStorage.getItem("id");
+    }, // store the id in localstorage
+    username: function() {
+      return localStorage.getItem("username");
+    } // store the username in localstorage
+  },
+  mounted() {
+    this.obtendatos();
+  },
+  methods: {
+    obtendatos() {
+      api
+        .get("/api/" + this.tabla.tabla + "/", {
+          headers: { "x-access-token": this.JWTToken }
+        })
+        .then(response => {
+          this.data = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    addRow() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.data[this.editedIndex], this.editedItem);
+      } else {
+        this.data.push(this.editedItem);
+      }
+      this.close();
+    },
+    deleteItem(item) {
+      const id = item.id;
+      confirm("Are you sure you want to delete this item?") &&
+        this.borraDatoTabla(id);
+    },
+    borraDatoTabla(id) {
+      api
+        .delete("/api/" + this.tabla.tabla + "/id?id=" + id, {
+          headers: { "x-access-token": this.JWTToken }
+        })
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      this.obtendatos();
+    },
+    editItem(item) {
+      this.editedIndex = this.data.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.show_dialog = true;
+    },
+    close() {
+      this.show_dialog = false;
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
+    }
+  }
+};
+</script>
--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -1,15 +1,31 @@
 <!-- Aqui template, donde ira el HTML que Vue renderizara -->
 <template>
-  <div class="fixed-center text-center"></div>
+  <div class="row">
+    <div
+      class="col-sm-4 col-xs-12 q-pa-sm q-mt-xl"
+      v-for="(tabla, key) in datos"
+      v-bind:key="key"
+    >
+      <editortablas v-bind:tabla="tabla"></editortablas>
+    </div>
+  </div>
 </template>
 <!-- Aqui script, donde irá el Javascript (métodos, funciones, etc) -->
 <script>
+import editortablas from "../components/EditorTablas.vue";
 export default {
   name: "Administracion",
-  data: function() {
-    return {};
+  components: {
+    editortablas
   },
-
-  methods: {}
+  data() {
+    return {
+      datos: [
+        { nombre: "Departamentos", tabla: "departamento" },
+        { nombre: "Estados", tabla: "estadodia" },
+        { nombre: "Roles", tabla: "rol" }
+      ]
+    };
+  }
 };
 </script>
